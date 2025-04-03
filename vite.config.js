34b34ef8d2ee3c3fed9f04a20067065fa768b790
Jonathan Barroso <barrosojonathan02@gmail.com>
2025-04-03 21:33:22 +0100

    nuevos cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/calendario-psicologa/", // Asegura que las rutas sean correctas
+  base: "/calendario-psicologa/", // Cambia esto según el nombre de tu repo
 });
